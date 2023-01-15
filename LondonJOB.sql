de85399bd9045f9db8529f40f421d5b65f2de793
Davide87ITA <122736927+Davide87ITA@users.noreply.github.com>
2023-01-15 19:47:34 +0100

    Update LondonJOB.sql

--- LondonJOB.sql
+++ LondonJOB.sql
@@ -1,11 +1,21 @@
 --extracting the 10 industries which the most increases of jobs --
 select top 10 industry, jobs_2011, jobs_2014, sum(change) as change
 FROM [jobEng].[dbo].[JobUK]
-group by change, industry, city, jobs_2011, jobs_2014
+WHERE City = 'London'
+group by change, industry, jobs_2011, jobs_2014
 order by change
 
 -- doing the same for the 10 industries who had the most decrease of jobs --
-select industry, city, max(jobs_2011), jobs_2014, sum(change) as change
+select top 10 industry, jobs_2011, jobs_2014, sum(change) as change
+WHERE City = 'London'
 FROM [jobEng].[dbo].[JobUK]
 group by change, industry, city, jobs_2011, jobs_2014
 order by change asc
+
+--percentage--
+
+select industry, jobs_2011, jobs_2014, change, (change/Jobs_2011)*100 as 'percentage'
+FROM [jobEng].[dbo].[JobUK]
+where City = 'London' and Jobs_2011 != 0 
+group by change, industry, city, jobs_2011, jobs_2014
+order by change desc
