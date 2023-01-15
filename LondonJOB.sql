1e33efea9afacec61858b44b15c3c8d14654dd13
Davide87ITA <122736927+Davide87ITA@users.noreply.github.com>
2023-01-15 16:34:25 +0100

    Update LondonJOB.sql

--- LondonJOB.sql
+++ LondonJOB.sql
@@ -1,28 +1,11 @@
-/****** Script per comando SelectTopNRows da SSMS  ******/
-SELECT  top 20 industry, jobs_2011, jobs_2014, change
-  FROM [jobEng].[dbo].[JobUK]
-WHERE change > 50
-order by change desc
-
-
-SELECT top 20 city, industry,SIC_1_name, jobs_2011, jobs_2014, change
-  FROM [jobEng].[dbo].[JobUK]
-order by change asc
-
-
-/* JUST CALLING OUT EVERYTHING*/
-Select *
+--extracting the 10 industries which the most increases of jobs --
+select top 10 industry, jobs_2011, jobs_2014, sum(change) as change
 FROM [jobEng].[dbo].[JobUK]
-
-/*TRY TO SUM THE INDUSTRY AND ORDER RESULTS BY NUMBER OF CHANGE*/
-select industry, city, sum(jobs_2011) as jobs2011, sum(jobs_2014) as jobs2014, sum(change) as change
-FROM [jobEng].[dbo].[JobUK]
-where change < - 100
-group by change, industry, city
-order by change
-
-select industry, city, max(jobs_2011), jobs_2014, sum(change) as change
-FROM [jobEng].[dbo].[JobUK]
-where change < - 100
 group by change, industry, city, jobs_2011, jobs_2014
 order by change
+
+-- doing the same for the 10 industries who had the most decrease of jobs --
+select industry, city, max(jobs_2011), jobs_2014, sum(change) as change
+FROM [jobEng].[dbo].[JobUK]
+group by change, industry, city, jobs_2011, jobs_2014
+order by change asc
